4516d90ed4d7e5227a65c2a5e12f752039c3ac9a
Hekkipekki <dannor1992@gmail.com>
2026-07-04 15:28:40 +0200

    Route plot clicks through event-aware handler

--- gui/events/__init__.py
+++ gui/events/__init__.py
@@ -1,2 +1,2 @@
-from gui.events.click_handlers import onclick, on_space_key_pressed
+from gui.events.plot_clicks import onclick, on_space_key_pressed
 from gui.events.event_finalize import finalize_event
