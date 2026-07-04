c9226e5b9ff9e1c95651f60f47e73b36cd9ead2a
Hekkipekki <dannor1992@gmail.com>
2026-07-04 16:13:26 +0200

    Refactor popup positioning helpers

--- gui/events/popup_utils.py
+++ gui/events/popup_utils.py
@@ -1,9 +1,26 @@
-def center_popup_in_canvas(app, width=260, height=220, top_offset=150):
+DEFAULT_POPUP_WIDTH = 260
+DEFAULT_POPUP_HEIGHT = 220
+DEFAULT_CENTER_TOP_OFFSET = 150
+DEFAULT_PREPARE_TOP_OFFSET = 400
+
+
+def _canvas_bounds(app):
     canvas_widget = app.canvas.get_tk_widget()
     canvas_widget.update_idletasks()
-    x_canvas = canvas_widget.winfo_rootx()
-    y_canvas = canvas_widget.winfo_rooty()
-    canvas_width = canvas_widget.winfo_width()
+    return (
+        canvas_widget.winfo_rootx(),
+        canvas_widget.winfo_rooty(),
+        canvas_widget.winfo_width(),
+    )
+
+
+def center_popup_in_canvas(
+    app,
+    width=DEFAULT_POPUP_WIDTH,
+    height=DEFAULT_POPUP_HEIGHT,
+    top_offset=DEFAULT_CENTER_TOP_OFFSET,
+):
+    x_canvas, y_canvas, canvas_width = _canvas_bounds(app)
     x = x_canvas + (canvas_width - width) // 2
     y = y_canvas + top_offset
     return x, y
@@ -13,7 +30,13 @@ def format_geometry_string(width, height, x, y):
     return f"{width}x{height}+{x}+{y}"
 
 
-def prepare_popup(popup, app, width=260, height=220, top_offset=400):
+def prepare_popup(
+    popup,
+    app,
+    width=DEFAULT_POPUP_WIDTH,
+    height=DEFAULT_POPUP_HEIGHT,
+    top_offset=DEFAULT_PREPARE_TOP_OFFSET,
+):
     popup.withdraw()
     popup.update_idletasks()
     x, y = center_popup_in_canvas(app, width, height, top_offset)
